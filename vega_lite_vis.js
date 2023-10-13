81c9e68f7647ee30d42f5f69569ff47d943594d1
SY-253 <94880156+SY-253@users.noreply.github.com>
2023-10-13 12:59:42 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,7 @@
 var vg_1 = "map.json";
 var vg_2 = "bubble.json";
+var vg_3 = "donut.json";
 
 vegaEmbed("#map", vg_1).then(function(result){}).catch(console.error);
 vegaEmbed("#bubble", vg_2).then(function(result){}).catch(console.error);
+vegaEmbed("#donut", vg_3).then(function(result){}).catch(console.error);
